ba7104025bb84c630f5af37c13da2f1db633ff34
MJ <mj045658@student.ath.edu.pl>
2018-05-09 15:16:07 +0200

    Add project files.

---
+++ Photos/Controllers/FileShareController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Photos.Controllers
+{
+    public class FileShareController : Controller
+    {
+        // GET: FileShare
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        // POST: FileShare/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
